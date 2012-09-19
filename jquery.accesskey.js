f892f14165bb480dacb33942591fe1bb0c1665ab
Gabriel Izaias <gabriel.izaias@gmail.com>
2012-09-19 20:12:54 -0300

    Complete rewrite

--- jquery.accesskey.js
+++ jquery.accesskey.js
@@ -7,57 +7,54 @@
  * @author      Gabriel Izaias <gabriel.izaias@gmail.com>
  */
 ;(function($){
+    "use strict";
+
     var ua = navigator.userAgent.toLowerCase(),
         os = detect('os'),
-        browser = detect('browser');
-        newTitle = '';
+        browser = detect('browser'),
+        keystrokes = {
+            windows : {
+                ie      : '[alt+%s]',
+                chrome  : '[alt+%s]',
+                safari  : '[alt+%s]',
+                firefox : '[alt+shift+%s]',
+                opera   : '[shif+esc+%s]'
+            },
+            machintosh : {
+                chrome  : '[ctrl+alt+%s]',
+                safari  : '[ctrl+alt+%s]',
+                firefox : '[ctrl+alt+%s]',
+                opera   : '[shif+esc+%s]'
+            }
+        }
 
-    console.log( browser, 'on', os );
-    // Loo trought every element that has a accesskey attribute defined
+    // Loop trought every element that has a accesskey attribute defined
     $('[accesskey]').each(function(){
-        var accesskey = this.accessKey,
-            newTitle = title = this.title || '';
-
-        // switch(os){
-        //     case 'windows' :
-        //         newTitle = $.browser.msie || $.browser.webkit ? title + ' [alt+' + accesskey + ']' :
-        //              $.browser.opera ? title + ' [shift+esc+' + accesskey + ']' :
-        //              $.browser.mozilla ? title + ' [alt+shift+' + accesskey + ']' :
-        //              title;
-        //         break;
-        //     case 'machintosh' :
-        //         newTitle = $.browser.msie || $.browser.webkit ? title + ' [cmd+alt+' + accesskey + ']' :
-        //              $.browser.opera ? title + ' [shift+esc+' + accesskey + ']' :
-        //              $.browser.mozilla ? title + ' [cmd+' + accesskey + ']' :
-        //              title;
-        //         break;
-        //     default:
-        // }
-
-        return this.title = newTitle;
+        var accesskey = this.accessKey.split(' ') || ''
+        this.title += ' ' + keystrokes[os][browser].replace( /%s/, accesskey[0] )
     });
 
     /**
      * Run any detection needed
      * 
-     * @param  String what What you want to be detected
-     * @return Mixed
+     * @param  string what What you want to be detected
+     * @return mixed
      */
     function detect( what ){
         switch ( what ) {
             case 'browser' :
-                var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua) || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(ie) ([\w.]+)/.exec(ua) || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua) || []
+                var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua) 
+                    || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua) 
+                    || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) 
+                    || /(ie) ([\w.]+)/.exec(ua) 
+                    || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua) 
+                    || []
                 return browserArray[1] || undefined;
-                break;
             case 'os' :
                 return ua.indexOf('macintosh') !== -1 ? 'machintosh' :
                        ua.indexOf('windows') !== -1 ? 'windows' :
                        ua.indexOf('linux') !== -1 ? 'linux' :
                        undefined;
-                break;
-            default :
-                return;
-                break;
         }
     }
 })(jQuery);
