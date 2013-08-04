856c0a0e962ae219a1692be857a41db6ff981c21
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-08-04 11:02:39 -0300

    Fix typo

--- jquery.accesskey.js
+++ jquery.accesskey.js
@@ -1,9 +1,9 @@
  /**
  * Accesskey user support
- * 
- * Adds a hint to the title attribute of elements that support 
+ *
+ * Adds a hint to the title attribute of elements that support
  * accesskey shortcuts
- * 
+ *
  * @author      Gabriel Izaias <gabriel.izaias@gmail.com>
  */
 ;(function($){
@@ -28,7 +28,7 @@
             }
         }
 
-    // Loop trought every element that has a accesskey attribute defined
+    // Loop trought every element that has an accesskey attribute defined
     $('[accesskey]').each(function(){
         var accesskey = this.accessKey.split(' ') || ''
         this.title += ' ' + keystrokes[os][browser].replace( /%s/, accesskey[0] )
@@ -36,18 +36,18 @@
 
     /**
      * Run any detection needed
-     * 
+     *
      * @param  string what What you want to be detected
      * @return mixed
      */
     function detect( what ){
         switch ( what ) {
             case 'browser' :
-                var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua) 
-                    || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua) 
-                    || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) 
-                    || /(ie) ([\w.]+)/.exec(ua) 
-                    || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua) 
+                var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua)
+                    || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua)
+                    || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua)
+                    || /(ie) ([\w.]+)/.exec(ua)
+                    || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua)
                     || []
                 return browserArray[1] || undefined;
             case 'os' :
