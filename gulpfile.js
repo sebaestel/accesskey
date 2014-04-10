87cb88353a08571b164b922d9811e10d9b117b42
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-04-10 18:09:57 -0300

    Add gulp-bump tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    bump = require('gulp-bump');
 
 gulp.task('compress', function () {
     "use strict";
@@ -9,6 +10,31 @@ gulp.task('compress', function () {
         .pipe(rename('accesskey.min.js'))
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('bump', function (params) {
+    "use strict";
+    gulp.src(['./package.json', './urlToLink.jquery.json'])
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+})
+
+gulp.task('bump:minor', function () {
+    "use strict";
+    gulp.src(['./package.json', './urlToLink.jquery.json'])
+        .pipe(bump({
+            type : 'minor'
+        }))
+        .pipe(gulp.dest('./'));
+})
+
+gulp.task('bump:major', function () {
+    "use strict";
+    gulp.src(['./package.json', './urlToLink.jquery.json'])
+        .pipe(bump({
+            type : 'major'
+        }))
+        .pipe(gulp.dest('./'));
+})
 
 gulp.task('default', function () {
     "use strict";
