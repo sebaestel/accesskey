f091aca78ab8249339b0a652ed1f9a14c7cc72bb
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-06-06 17:48:09 -0300

    Add browser detection

--- spec/accesskeySpec.js
+++ spec/accesskeySpec.js
@@ -1,3 +1,38 @@
 describe('Accesskey.js', function(){
+    describe('Browser detection', function() {
+        // User Agent used on the browser tests
+        var ua = window.navigator.userAgent.toLowerCase();
 
+        it('should equal to `chrome` when on Google Chrome', function(){
+            if (ua.indexOf('chrome') > 0) {
+                expect(Accesskey.getBrowser()).toEqual('chrome');
+            } else {
+                 expect(Accesskey.getBrowser()).not.toEqual('chrome');
+            }
+        });
+
+        it('should equal to `firefox` when on Mozilla Firefox', function(){
+            if (ua.indexOf('firefox') > 0) {
+                expect(Accesskey.getBrowser()).toEqual('firefox');
+            } else {
+                 expect(Accesskey.getBrowser()).not.toEqual('firefox');
+            }
+        });
+
+        it('should equal to `safari` when on Apple Safari', function(){
+            if (ua.indexOf('safari') > 0 && ua.indexOf('chrome') < 0) {
+                expect(Accesskey.getBrowser()).toEqual('safari');
+            } else {
+                 expect(Accesskey.getBrowser()).not.toEqual('safari');
+            }
+        });
+
+        it('should equal to `opera` when on Opera', function(){
+            if (ua.indexOf('opera') > 0) {
+                expect(Accesskey.getBrowser()).toEqual('opera');
+            } else {
+                 expect(Accesskey.getBrowser()).not.toEqual('opera');
+            }
+        });
+    });
 });
--- src/accesskey.js
+++ src/accesskey.js
@@ -1,76 +1,106 @@
-/**
- * Accesskey user support
- *
- * Adds a hint to the title attribute of elements that support
- * accesskey shortcuts
- *
- * @author      Gabriel Izaias <gabriel.izaias@gmail.com>
- */
-!(function (window, undefined) {
-    "use strict";
-
-    var
-        /**
-         * User Agent
-         *
-         * @type {string}
-         */
-        ua = window.navigator.userAgent.toLowerCase(),
-
-        /**
-         * Keystroke combination for every browser
-         *
-         * @type {Object}
-         */
-        keystrokes = {
-            windows : {
-                ie      : '[alt+%s]',
-                chrome  : '[alt+%s]',
-                safari  : '[alt+%s]',
-                firefox : '[alt+shift+%s]',
-                opera   : '[shift+esc+%s]'
-            },
-            machintosh : {
-                chrome  : '[ctrl+alt+%s]',
-                safari  : '[ctrl+alt+%s]',
-                firefox : '[ctrl+alt+%s]',
-                opera   : '[shift+esc+%s]'
-            },
-            linux : {
-                chrome  : '[alt+%s]',
-                firefox : '[alt+shift+%s]',
-                opera   : '[shift+esc+%s]'
-            }
-        },
-
-        /**
-         * Operating System
-         *
-         * @type {string}
-         */
-        os = (function () {
-            return ua.indexOf('macintosh') !== -1 ? 'machintosh' :
-                   ua.indexOf('windows') !== -1 ? 'windows' :
-                   ua.indexOf('linux') !== -1 ? 'linux' :
-                   undefined;
-        }()),
-
-        /**
-         * Browser
-         *
-         * @type {string}
-         */
-        browser = (function () {
-            var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua) || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(ie) ([\w.]+)/.exec(ua) || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua) || []
-            return browserArray[1] || undefined;
-        }()),
-        items = window.document.querySelectorAll('[accesskey]') || [],
-        accesskey,
-        i;
-
-    for (i = items.length - 1; i >= 0; i--) {
-        accesskey = items[i].accessKey.split(' ') || ''
-        items[i].title += ' ' + keystrokes[os][browser].replace(/%s/, accesskey[0])
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['b'], function (b) {
+            return (root.Accesskey = factory(b));
+        });
+    } else {
+        // Browser globals
+        root.Accesskey = factory(root.b);
     }
+}(this, function (b) {
+/**
+ * User Agent
+ * @type {String}
+ */
+    var ua = window.navigator.userAgent.toLowerCase();
 
-})(window);
+/**
+ * An array containing the browser lowercase name and its version
+ * @type {RegExp}
+ */
+    var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua) || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(ie) ([\w.]+)/.exec(ua) || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua) || []
+
+    return {
+        getBrowser : function () {
+            return browserArray[1] || undefined;
+        }
+    };
+}));
+
+// /**
+//  * Accesskey user support
+//  *
+//  * Adds a hint to the title attribute of elements that support
+//  * accesskey shortcuts
+//  *
+//  * @author      Gabriel Izaias <gabriel.izaias@gmail.com>
+//  */
+// !(function (window, undefined) {
+//     "use strict";
+
+//     var
+//         /**
+//          * User Agent
+//          *
+//          * @type {string}
+//          */
+//         ua = window.navigator.userAgent.toLowerCase(),
+
+//         /**
+//          * Keystroke combination for every browser
+//          *
+//          * @type {Object}
+//          */
+//         keystrokes = {
+//             windows : {
+//                 ie      : '[alt+%s]',
+//                 chrome  : '[alt+%s]',
+//                 safari  : '[alt+%s]',
+//                 firefox : '[alt+shift+%s]',
+//                 opera   : '[shift+esc+%s]'
+//             },
+//             machintosh : {
+//                 chrome  : '[ctrl+alt+%s]',
+//                 safari  : '[ctrl+alt+%s]',
+//                 firefox : '[ctrl+alt+%s]',
+//                 opera   : '[shift+esc+%s]'
+//             },
+//             linux : {
+//                 chrome  : '[alt+%s]',
+//                 firefox : '[alt+shift+%s]',
+//                 opera   : '[shift+esc+%s]'
+//             }
+//         },
+
+//         /**
+//          * Operating System
+//          *
+//          * @type {string}
+//          */
+//         os = (function () {
+//             return ua.indexOf('macintosh') !== -1 ? 'machintosh' :
+//                    ua.indexOf('windows') !== -1 ? 'windows' :
+//                    ua.indexOf('linux') !== -1 ? 'linux' :
+//                    undefined;
+//         }()),
+
+//         /**
+//          * Browser
+//          *
+//          * @type {string}
+//          */
+//         browser = (function () {
+//             var browserArray = /(chrome)(?:.*chrome)?[ \/]([\w.]+)/.exec(ua) || /(safari)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(ie) ([\w.]+)/.exec(ua) || !/compatible/.test(ua) && /(firefox)(?:.*firefox)?[ \/]([\w.]+)/.exec(ua) || []
+//             return browserArray[1] || undefined;
+//         }()),
+//         items = window.document.querySelectorAll('[accesskey]') || [],
+//         accesskey,
+//         i;
+
+//     for (i = items.length - 1; i >= 0; i--) {
+//         accesskey = items[i].accessKey.split(' ') || ''
+//         items[i].title += ' ' + keystrokes[os][browser].replace(/%s/, accesskey[0])
+//     }
+
+// })(window);
